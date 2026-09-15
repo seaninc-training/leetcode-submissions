0db411925f10e9d05d24cd1d633ad9de1b25becc
Sean Incardona <sinc7train@gmail.com>
2026-09-15 12:08:30 -0400

    Time: 727 ms (86.27%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,15 +1,8 @@
-WITH feb_totals AS (
     SELECT 
         p.product_name,
         SUM(o.unit) AS unit
     FROM Orders o
     LEFT JOIN Products p ON o.product_id = p.product_id
-    WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
+    WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
     GROUP BY o.product_id
-)
-
-SELECT 
-    product_name, 
-    unit
-FROM feb_totals
-WHERE unit >= 100
+    HAVING SUM(o.unit) >= 100
